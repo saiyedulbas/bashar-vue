c1ac10909484b8e7987be0150a75116963ff5d93
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 19:31:45 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const serveStatic = require('serve-static');
+            const express = require('express');
+                  const serveStatic = require('serve-static');
 
-const path = require('path');
+                  const path = require('path');
 
-const app = express();
+      const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+                      app.use('/', serveStatic(path.join(__dirname, '/dist')));
+                      const PORT = process.env.PORT || 5000;
+                      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
